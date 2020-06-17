2e4ce7d5a7b5d8872861e1222bc369b75c8ac5e6
LoneStar <ernieurzua@gmail.com>
2020-06-17 11:57:38 -0700

    guess of how to relate data

--- 03-javascript/activities/29-JoanOfArcArrays/Unsolved/joan-of-arc-arrays-unsolved.js
+++ 03-javascript/activities/29-JoanOfArcArrays/Unsolved/joan-of-arc-arrays-unsolved.js
@@ -9,3 +9,17 @@ var joanOfArcInfoValues = [
   "Took an arrow to the shoulder and a crossbow bolt to the thigh while trying to liberate Paris.",
   "Stands for French unity and nationalism."
 ];
+
+//im thinkig of array list, or a map
+// e.g. Real Name maps to joanofArchInfoValues[0]
+//or some way to say joanOfArcInfoParts[0] maps to joanOfArcInfoValues[0]
+//or a struct of some sort 
+
+// joanofArchInfoParts {
+//   Real Name, "jehanne la pucelle"
+//   Grew up Where, "Jehanne la Pucelle.",
+//   "Domremy, a village in northeastern France.",
+//   "Peasant girl, daughter of a farmer, who rose to become Commander of the French army.",
+//   "Took an arrow to the shoulder and a crossbow bolt to the thigh while trying to liberate Paris.",
+//   "Stands for French unity and nationalism."
+// }
