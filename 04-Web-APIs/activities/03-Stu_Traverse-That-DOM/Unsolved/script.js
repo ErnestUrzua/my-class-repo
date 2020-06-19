07a5b5fb05bcbb7cc128e6fb48000e78abe75a17
LoneStar <ernieurzua@gmail.com>
2020-06-18 17:12:28 -0700

    added dom traversal

---
+++ 04-Web-APIs/activities/03-Stu_Traverse-That-DOM/Unsolved/script.js
@@ -0,0 +1,6 @@
+document.body.children[1].style.fontSize = "50px"; //access articles
+document.body.children[1].firstChild.style.color = "red"; //access articles
+
+//document.body.childNodes.style.color = "blue";
+
+document.body.children[1].lastElementChild.style.color = "blue";
