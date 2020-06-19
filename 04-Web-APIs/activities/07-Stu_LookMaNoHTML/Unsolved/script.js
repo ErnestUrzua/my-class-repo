12b42fffbb472d5866526e13d8ebf40c79ec8ff6
LoneStar <ernieurzua@gmail.com>
2020-06-18 17:14:03 -0700

    added header via Dom method

--- 04-Web-APIs/activities/07-Stu_LookMaNoHTML/Unsolved/script.js
+++ 04-Web-APIs/activities/07-Stu_LookMaNoHTML/Unsolved/script.js
@@ -1,1 +1,12 @@
 // Create your HTML Page via DOM Methods here!
+var h1;
+var header = document.createElement(h1);
+  header.textContent = "This is a centered header " + h1;
+  header.style.justifyContent = "center"; 
+  document.body.appendChild(header);
+
+// var img;
+// var image = document.createElement(img);
+// image.textContent = "This is our second tag via prompts, it's a " + image;
+// document.body.appendChild(image);
+  
