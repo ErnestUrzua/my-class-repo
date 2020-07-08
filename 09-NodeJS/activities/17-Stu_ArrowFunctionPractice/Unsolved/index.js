2995f46422b3e6675413be6afd556eeb9c787eb6
LoneStar <ernieurzua@gmail.com>
2020-07-08 14:39:06 -0700

    added new content

--- 09-NodeJS/activities/17-Stu_ArrowFunctionPractice/Unsolved/index.js
+++ 09-NodeJS/activities/17-Stu_ArrowFunctionPractice/Unsolved/index.js
@@ -1,12 +1,11 @@
 // 1. funnyCase makes each letter in a string the opposite case as the one before
-var funnyCase = function(string) {
+var funnyCase = (string) => 
   var newString = "";
   for (var i = 0; i < string.length; i++) {
     if (i % 2 === 0) newString += string[i].toLowerCase();
     else newString += string[i].toUpperCase();
   }
-  return newString;
-};
+;
 
 // Prints `yOu cAn't jUsT Do wHaTeVeR YoU WaNt aLl tHe tImE!`
 console.log(funnyCase("You can't just do whatever you want all the time!"));
