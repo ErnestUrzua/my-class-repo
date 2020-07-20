b54284f9fbb0ea457b993467881cb69f88548179
LoneStar <ernieurzua@gmail.com>
2020-07-20 13:25:54 -0700

    updated notes and updated sql files

--- 12-MySQL/activities/02-FavoriteDB-NoData/Unsolved/favorite_db_unsolved.sql
+++ 12-MySQL/activities/02-FavoriteDB-NoData/Unsolved/favorite_db_unsolved.sql
@@ -6,19 +6,31 @@ CREATE DATABASE favorite_db;
 -- Make it so all of the following code will affect favorite_db --
 
 -- Creates the table "favorite_foods" within favorite_db --
-CREATE TABLE favorite_foods (
+CREATE TABLE favorite_foods
+(
+  food VARCHAR (50) NOT NULL,
+  score INTEGER(3)
   -- Make a string column called "food" which cannot contain null --
   -- Make an numeric column called "score" --
 );
 
-CREATE TABLE favorite_songs (
+CREATE TABLE favorite_songs
+(
+  songs VARCHAR (50) NOT NULL,
+  artist VARCHAR (50) NOT NULL,
+  score INTEGER(3)
   -- Make a string column called "song" which cannot contain null --
   -- Make a string column called "artist" --
   -- Make an integer column called "score" --
-  score INTEGER(10)
 );
 
-CREATE TABLE favorite_movies (
+CREATE TABLE favorite_movies
+(
+  id INTEGER NOT NULL
+  AUTO_INCREMENT,
+  movie VARCHAR (100) NOT NULL,
+five_times BOOLEAN DEFAULT false,
+PRIMARY KEY (id)
   -- Create a numeric column called "id" which automatically increments and cannot be null --
   -- Create a string column called "movie" which cannot be null --
   -- Create a boolean column called "five_times" that sets the default value to false if nothing is entered --
--- 12-MySQL/activities/04-moreDB/02-books/books.sql
+++ 12-MySQL/activities/04-moreDB/02-books/books.sql
@@ -3,7 +3,47 @@ CREATE DATABASE library_db;
 USE library_db;
 
 CREATE TABLE books(
-
+id INTEGER(10) AUTO_INCREMENT NOT NULL,
+title VARCHAR(50) NOT NULL,
+rating INTEGER(3),
+PRIMARY KEY (id)
 );
+
+INSERT INTO books (title,rating)
+VALUES ("LOTR",85);
+
+INSERT INTO books (title,rating)
+VALUES ("Twilight",30);
+
+INSERT INTO books (title,rating)
+VALUES ("Rome",69);
+
+UPDATE books
+SET title = "The Lord Of The Rings"
+WHERE id = 1;
+
+CREATE TABLE authors(
+id INTEGER(10) AUTO_INCREMENT NOT NULL,
+firstName VARCHAR(50),
+lastName VARCHAR(50),
+PRIMARY KEY (id) 
+);
+
+INSERT INTO authors (firstName,lastName)
+VALUES ("JRR","Tolken");
+
+INSERT INTO authors (firstName,lastName)
+VALUES ("Stephen","King");
+
+INSERT INTO authors (firstName,lastName)
+VALUES ("Dean","Koontz");
+
+
+UPDATE authors
+SET firstName = "J.R.R"
+WHERE id = 1;
+
+
+SELECT * FROM authors;
 
 SELECT * FROM books;
