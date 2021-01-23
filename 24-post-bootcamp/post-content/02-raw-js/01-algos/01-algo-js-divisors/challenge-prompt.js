b57df5308209cc5cec35fd9ef60503b0b4551703
LoneStar <ernieurzua@gmail.com>
2021-01-22 19:57:30 -0800

    created function that checks if elements are divisors of each other.

--- 24-post-bootcamp/post-content/02-raw-js/01-algos/01-algo-js-divisors/challenge-prompt.js
+++ 24-post-bootcamp/post-content/02-raw-js/01-algos/01-algo-js-divisors/challenge-prompt.js
@@ -7,11 +7,15 @@
 function checkDivisors(arr) {
 
   // ---------- Your Code Here ----------
-
-
-
-
-
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (i !== j && arr[i] % arr[j] == 0) {
+        //console.log('true ' + i + ' ' + j)
+        return true;
+      }
+    }
+  }
+  return false;
 
   // ----------- End Code Area -----------
 
